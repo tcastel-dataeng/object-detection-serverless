cf4406457e4d719cd12b2bb74ddeb48a44de04a5
Théo CASTEL <theocastel@Theos-MacBook-Pro.local>
2019-10-29 10:25:12 +0100

    First commit

---
+++ lambda_function.py
@@ -0,0 +1,175 @@
+import json
+import urllib.parse
+import boto3
+import cv2
+import numpy as np
+import os
+import sys
+import time
+
+
+def load_file_content_from_S3 (bucket_name,key):
+        file_obj = s3.get_object(Bucket=bucket_name, Key=key)
+        file_content = file_obj["Body"].read()
+        return file_content
+
+def list_S3_object(bucket_name):
+    print(s3.list_objects_v2(Bucket=bucket_name))
+
+def read_image_from_S3(bucket_name,key):
+        file_content = load_file_content_from_S3(bucket_name,key)
+        np_array = np.fromstring(file_content, np.uint8)
+        return cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+def download_temporary_file_from_S3(bucket_name,key):
+
+    localFilename = '/tmp/{}'.format(os.path.basename(key))
+    try:    
+    s3.download_file(Bucket=bucket_name, Key=key, Filename=localFilename_cfg) 
+    return localFilename
+    except Exception as e:
+        print(e)
+        print('Error downloading  object {} in the bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket_name))
+        raise e
+    
+
+    
+    localFilename_cfg = '/tmp/{}'.format(os.path.basename("models/yolov3.cfg"))
+    localFilename_weights = '/tmp/{}'.format(os.path.basename("models/yolov3.weights"))
+    
+    s3.download_file(Bucket=bucket_name, Key="models/yolov3-tiny.cfg", Filename=localFilename_cfg)
+    s3.download_file(Bucket=bucket_name, Key="models/yolov3-tiny.weights", Filename=localFilename_weights)
+
+
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    
+    def get_output_layers(net):
+    
+        layer_names = net.getLayerNames()
+        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+        return output_layers
+
+     def download_model_classes_from_s3(bucket_name,key):
+
+        file_classes = load_file_content_from_S3(bucket_name,key)
+        return file_classes.decode("utf-8").split("\n")
+
+
+    def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+        label = str(classes[class_id]) + str(format(confidence * 100, '.2f')) + '%'
+        color = COLORS[class_id]
+
+        cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+        cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+    def draw_bounding_boxes(image,boxes,indices):
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+    
+    start = time.time()
+    
+    # Get the object from the event and show its content type
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    ############################################################################
+    image = read_image_from_S3(bucket_name,key)
+
+    
+    getimage = time.time()   
+    print("time to get image :",getimage - start)
+    
+    classes = download_model_classes_from_s3(bucket_name,"models/yolov3.txt")
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    
+    
+    getclasses = time.time()   
+    print("time to create classes :",getclasses-getimage)
+    
+
+    localFilename_cfg = download_temporary_file_from_S3(bucket_name,"models/yolov3-tiny.cfg")
+    localFilename_weights = download_temporary_file_from_S3(bucket_name,"models/yolov3-tiny.weights")
+    
+    getdl = time.time()   
+    print("time to download files :",getdl-getclasses)
+    
+    net = cv2.dnn.readNet(localFilename_weights, localFilename_cfg)
+    
+    
+
+    
+    ##########################################################################
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
+    
+    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+    net.setInput(blob)
+    
+    createmodel = time.time()   
+    print("time to create model :",createmodel-getdl)
+
+    
+    outs = net.forward(get_output_layers(net))
+    
+    applymodel = time.time()   
+    print("time to applymodel :",applymodel-createmodel)
+    
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+    
+    
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+    
+    
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    
+
+    draw_bounding_boxes(image,boxes,indices)
+
+    getbox = time.time()   
+    print("time to draw boxes :",getbox-applymodel)    
+        
+    try:
+        filename, file_extension = os.path.splitext(key)
+        
+        new_key = "transformedImages/"+filename[7:]+"ObjectDetection"+file_extension
+        response = s3.put_object(Body=cv2.imencode('.jpg', image)[1].tostring(), Bucket=bucket_name, Key=new_key)
+        putobject = time.time()   
+        print("time to put object in s3 :",putobject-getbox) 
+        return response
+    except Exception as e:
+        print(e)
+        print('Error put in object {} in the bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket_name))
+        raise e
---
+++ tests.py
@@ -0,0 +1,83 @@
+import os
+import tempfile
+import unittest
+import boto3
+import botocore
+from moto import mock_s3
+import lambda_function
+import json
+
+MY_BUCKET = "my_bucket"
+MY_PREFIX = "mock_folder"
+
+@mock_s3
+class TestUseS3(unittest.TestCase):
+    def setUp(self):
+        client = boto3.client(
+            "s3",
+            region_name="us-east-1",
+            aws_access_key_id="access_key",
+            aws_secret_access_key="secret_key",
+            )
+        try:
+            s3 = boto3.resource(
+                "s3",
+                region_name="us-east-1",
+                aws_access_key_id="access_key",
+                aws_secret_access_key="secret_key",
+                )
+            s3.meta.client.head_bucket(Bucket=MY_BUCKET)
+        except botocore.exceptions.ClientError:
+            pass
+        else:
+            err = "{bucket} should not exist.".format(bucket=MY_BUCKET)
+            raise EnvironmentError(err)
+        client.create_bucket(Bucket=MY_BUCKET)
+        current_dir = os.path.dirname(__file__)
+        fixtures_dir = os.path.join(current_dir, MY_PREFIX)
+        _upload_fixtures(MY_BUCKET, fixtures_dir)
+    
+    def tearDown(self):
+        s3 = boto3.resource(
+            "s3",
+            region_name="us-east-1",
+            aws_access_key_id="access_key",
+            aws_secret_access_key="secret_key",
+            )
+        bucket = s3.Bucket(MY_BUCKET)
+        for key in bucket.objects.all():
+            key.delete()
+        bucket.delete()
+    
+    def test_load_file_content_from_S3(self):
+        testContent = lambda_function.load_file_content_from_S3(MY_BUCKET,"test.txt")
+        self.assertEqual(testContent.decode("utf-8"),"test")
+
+    def printfiles(self):
+        lambda_function.list_S3_object(MY_BUCKET)
+        self.assertEqual(1,1)
+
+    #def test_run_main(self):
+
+    #    with open('testEvent.json', 'r') as test_event_file:
+    #        event = json.load(test_event_file)
+
+    #    response = lambda_function.lambda_handler(event,0)
+    #    self.assertEqual(response["ResponseMetadata"]["HTTPStatusCode"],200)
+    
+
+
+def _upload_fixtures(bucket: str, fixtures_dir: str) -> None:
+        client = boto3.client("s3")
+        fixtures_paths = [
+            os.path.join(path,  filename)
+            for path, _, files in os.walk(fixtures_dir)
+            for filename in files
+        ]
+        for path in fixtures_paths:
+            key = os.path.relpath(path, fixtures_dir)
+            client.upload_file(Filename=path, Bucket=bucket, Key=key)
+    
+
+if __name__ == "__main__":
+    unittest.main()
