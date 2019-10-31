d9af4914fde3b20832b3b519793d28b6046570cd
tcastel <tcastel@ippon.fr>
2019-10-31 15:27:08 +0100

    change for pep8

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# 
+
+"""A simple lamnda that detect object on an image.
+ 
+ This lambda is triggered by an S3 event by uploading a photo to the S3.
+ It then detect object on the photo and write pertinent prediction on the 
+ photo with bounding boxes.
+ 
+ """
+
+
 import json
 import urllib.parse
 import boto3
@@ -10,38 +24,61 @@ from objectdetection import *
 
 
 def load_file_content_from_S3(bucket_name, key):
-        try:
-            file_obj = s3.get_object(Bucket=bucket_name, Key=key)
-            file_content = file_obj["Body"].read()
-            return file_content   
-        except Exception as e:
-            print(e)
-            print("""Error getting object {} in the bucket {}. Make sure they
-            exist and your bucket is in the same region as this function."""
-            .format(key, bucket_name))
-        raise        
+    
+
+    """Return the content of the file loaded."""
+
+
+    try:
+        file_obj = s3.get_object(Bucket=bucket_name, Key=key)
+        file_content = file_obj["Body"].read()
+        return file_content
+
+    except Exception as e:
+        print(e)
+        print("""Error getting object {} in the bucket {}. Make sure they
+                exist and your bucket is in the
+                same region as this function.""".format(key, bucket_name))
+    raise        
+
 
 def read_image_from_S3(bucket_name, key):
+
+
+    """Return an image object."""
+
+
         file_content = load_file_content_from_S3(bucket_name, key)
         np_array = np.frombuffer(file_content, np.uint8)
         return cv2.imdecode(np_array, cv2.IMREAD_COLOR)
 
+
 def download_temporary_file_from_S3(bucket_name, key):
+
+
+    """Download a file from S3 and return the filename."""
+
 
     localFilename = '/tmp/{}'.format(os.path.basename(key))
 
     if os.path.exists(localFilename):
         return localFilename
-    else :
+    else:
         try:    
-            s3.download_file(Bucket=bucket_name, Key=key, Filename=localFilename) 
+            s3.download_file(
+                Bucket=bucket_name,
+                Key=key, 
+                Filename=localFilename)
+                            
             return localFilename
         except Exception as e:
             print(e)
-            print("""Error downloading  object {} in the bucket {}. Make sure they 
-            exist and your bucket is in the same region as this function."""
+            print("""Error downloading  object {} in the bucket {}. 
+            Make sure they exist and your bucket is in the same 
+            region as this function."""
             .format(key, bucket_name))
             raise e
+
 
 def download_temporary_file_from_S3_bis(bucket_name, key):
 
@@ -52,7 +89,11 @@ def download_temporary_file_from_S3_bis(bucket_name, key):
     return f.name    
 
 
-def download_model_classes_from_S3(bucket_name, key):
+def load_model_classes_from_S3(bucket_name, key):
+
+
+    """Return a list with all the classes detected by the model"""
+
 
     file_classes = load_file_content_from_S3(bucket_name, key)
     return file_classes.decode("utf-8").split("\n")
@@ -60,32 +101,45 @@ def download_model_classes_from_S3(bucket_name, key):
 
 s3 = boto3.client('s3')
 
+
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
     
-    #Get the object from the event
+#   print("Received event: " + json.dumps(event, indent=2))
+    
+#   Get the object from the event
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse\
-                .unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+                .unquote_plus(
+                    event['Records'][0]['s3']['object']['key'],
+                    encoding='utf-8')
     
-    #Read image and prepare classes
+#   Read image and prepare classes
     image = read_image_from_S3(bucket_name, key)
-    classes = download_model_classes_from_S3(bucket_name, "models/yolov3.txt")
+    classes = load_model_classes_from_S3(bucket_name, "models/yolov3.txt")
     
-    #Load model
-    localFilename_cfg = download_temporary_file_from_S3(bucket_name, "models/yolov3-tiny.cfg")
-    localFilename_weights = download_temporary_file_from_S3(bucket_name, "models/yolov3-tiny.weights")
+#   Load model
+    localFilename_cfg = download_temporary_file_from_S3(
+                                                bucket_name, 
+                                                "models/yolov3-tiny.cfg")
+                                                    
+    localFilename_weights = download_temporary_file_from_S3(
+                                                bucket_name, 
+                                                "models/yolov3-tiny.weights")
     
-    #Detect objects in an image write the detection on the image
+#   Detect objects in an image write the detection on the image
     object_detection(image, classes, localFilename_weights, localFilename_cfg)
 
-    #Create a new key for the image
+#   Create a new key for the image
     filename, file_extension = os.path.splitext(key)
-    new_key = "transformedImages/"+filename[7:]+"ObjectDetection"+file_extension
-    
+    new_key = "transformedImages/"+filename[7:]+\
+                "ObjectDetection"+file_extension
+
     try:
         
-        response = s3.put_object(Body=cv2.imencode('.jpg', image)[1].tostring(), Bucket=bucket_name, Key=new_key)
+        response = s3.put_object(
+                        Body=cv2.imencode('.jpg', image)[1].tostring(),
+                        Bucket=bucket_name,
+                        Key=new_key)
     
         return response
     except Exception as e:
